54023ce04aada21c26642ffb52795c4cd8c259e5
mee <kettlesimzulator420@gmail.com>
2019-07-07 02:21:20 +0100

    Add project files.

---
+++ InternetToolsV6/Utils/CommandRunner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace InternetToolsV6.Utils
+{
+    public class CommandRunner : ICommand
+    {
+        private readonly Action<object> _TargetExecuteMethod;
+        private readonly Func<bool> _TargetCanExecuteMethod;
+
+        public CommandRunner(Action<object> executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public CommandRunner(Action<object> executeMethod, Func<bool> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+
+            if (_TargetCanExecuteMethod != null)
+            {
+                return _TargetCanExecuteMethod();
+            }
+
+            if (_TargetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter)
+        {
+            _TargetExecuteMethod?.Invoke(parameter);
+        }
+    }
+}
---
+++ InternetToolsV6/ViewModels/MainViewModel.cs
@@ -0,0 +1,117 @@
+﻿using InternetToolsV6.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetToolsV6.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Variables
+        //Commands
+        public CommandRunner SendPing           { get; private set; }
+        public CommandRunner ClearIPStatusList  { get; private set; }
+        public CommandRunner ClearDNSStatusList { get; private set; }
+
+        //Textbox variables or values... whatever oof
+        private string address;
+        public string Address
+        {
+            get => address; 
+            set
+            {
+                address = value; RaisePropertyChanged();
+            }
+        }
+
+        private double timeout;
+        public double Timeout
+        {
+            get => timeout;
+            set
+            {
+                timeout = value; RaisePropertyChanged();
+            }
+        }
+
+        private bool icmpChecked;
+        public bool ICMPChecked
+        {
+            get => icmpChecked;
+            set
+            {
+                icmpChecked = value; RaisePropertyChanged();
+            }
+        }
+
+
+        private bool tcpChecked;
+        public bool TCPChecked
+        {
+            get => tcpChecked;
+            set
+            {
+                tcpChecked = value; RaisePropertyChanged();
+            }
+        }
+
+
+        private bool udpChecked;
+        public bool UDPChecked
+        {
+            get => udpChecked;
+            set
+            {
+                udpChecked = value; RaisePropertyChanged();
+            }
+        }
+
+
+        private bool arpChecked;
+        public bool ARPChecked
+        {
+            get => arpChecked;
+            set
+            {
+                arpChecked = value; RaisePropertyChanged();
+            }
+        }
+
+        private int tcpPort;
+        public int TCPPort
+        {
+            get => tcpPort;
+            set
+            {
+                tcpPort = value; RaisePropertyChanged();
+            }
+        }
+
+        private int udpPort;
+        public int UDPPort
+        {
+            get => udpPort;
+            set
+            {
+                udpPort = value; RaisePropertyChanged();
+            }
+        }
+
+        #endregion
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName]string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
